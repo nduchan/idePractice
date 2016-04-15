81d04843a0d8e3f3b65757d6f4d5f25615ae2115
nduchan <nduchan@umich.edu>
2016-04-15 10:28:16 -0400

    first commit

--- test2/ViewController.swift
+++ test2/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myLabel: UILabel!
+    @IBAction func myButton(sender: AnyObject) {
+            myLabel.text = "Hello C4C$"
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +23,6 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
 
 }
 
